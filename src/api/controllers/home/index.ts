e9131569ca3990a6fd2c1518c5f4cbe3f5111a7b
bonaxl015 <bonaxl015@gmail.com>
2025-09-06 12:43:38 +0800

    feat(homepage): update routes for home page and css requests

--- src/api/controllers/home/index.ts
+++ src/api/controllers/home/index.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import path from 'path';
+
 import asyncHandler from '../../middlewares/asyncHandler';
 import { StatusCode } from '../../enums/statusCodes';
 
@@ -7,5 +9,16 @@ export const getHome = asyncHandler(async (
   res: Response,
   _next: NextFunction
 ) => {
-  res.status(StatusCode.SUCCESS).send('Test');
+  res.status(StatusCode.SUCCESS).render('home');
+});
+
+export const getHomeStyles = asyncHandler(async (
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  res
+    .status(StatusCode.SUCCESS)
+    .type('text/css')
+    .sendFile(path.join(__dirname, '../../../public/css/home/index.css'));
 });
